cbefe2067b243f967263215f6bc62af685c32795
M4RT1N <mjlynch35@yahoo.com>
2023-06-05 12:23:52 -0500

    Update styling

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
 function resizeAndDownload() {
     const imageInput = document.getElementById('imageInput');
-
+  
     if (imageInput.files && imageInput.files[0]) {
-        const fileReader = new FileReader();
-        fileReader.onload = function (event) {
+      const fileReader = new FileReader();
+      fileReader.onload = function (event) {
         const image = new Image();
         image.src = event.target.result;
         image.onload = function () {
-            const canvas = document.createElement('canvas');
-            const context = canvas.getContext('2d');
+          const canvas = document.createElement('canvas');
+          const context = canvas.getContext('2d');
     
-            // Define desired sizes for resizing
-            const sizes = [
+          // Set the canvas dimensions to match the original image
+          canvas.width = image.width;
+          canvas.height = image.height;
+  
+          // Preserve image quality during resizing
+          context.imageSmoothingEnabled = true;
+          context.drawImage(image, 0, 0, canvas.width, canvas.height);
+    
+          // Define desired sizes for resizing
+          const sizes = [
             { width: 2560, height: 1080 },
             { width: 1280, height: 720 },
-            { width: 640, height: 360 }
-            ];
+            { width: 640, height: 320 }
+          ];
     
-            // Create a ZIP file using JSZip library
-            const zip = new JSZip();
+          // Create a ZIP file using JSZip library
+          const zip = new JSZip();
     
-            sizes.forEach((size) => {
+          sizes.forEach((size) => {
             const { width, height } = size;
     
-            // Resize the image
-            canvas.width = width;
-            canvas.height = height;
-            context.drawImage(image, 0, 0, width, height);
+            // Create a new canvas for resizing each image
+            const resizedCanvas = document.createElement('canvas');
+            resizedCanvas.width = width;
+            resizedCanvas.height = height;
+            const resizedContext = resizedCanvas.getContext('2d');
     
-            // Convert the canvas image to a data URL
-            const resizedDataUrl = canvas.toDataURL('image/jpeg');
+            // Perform the resizing without losing quality
+            resizedContext.drawImage(canvas, 0, 0, width, height);
+    
+            // Convert the resized canvas image to a data URL
+            const resizedDataUrl = resizedCanvas.toDataURL('image/jpeg', 1.0);
     
             // Add the resized image to the ZIP file
             zip.file(`resized_${width}x${height}.jpg`, resizedDataUrl.substr(resizedDataUrl.indexOf(',') + 1), { base64: true });
-            });
+          });
     
-            // Generate the ZIP file
-            zip.generateAsync({ type: 'blob' }).then(function (content) {
+          // Generate the ZIP file
+          zip.generateAsync({ type: 'blob' }).then(function (content) {
             // Create a download link for the ZIP file
             const downloadLink = document.createElement('a');
             downloadLink.href = URL.createObjectURL(content);
@@ -44,10 +56,11 @@ function resizeAndDownload() {
     
             // Trigger the download
             downloadLink.click();
-            alert("Downloading...");
-            });
+          });
         };
-        };
-        fileReader.readAsDataURL(imageInput.files[0]);
+      };
+      fileReader.readAsDataURL(imageInput.files[0]);
+    }else {
+        alert("Please add an image");
     }
-}
+  }
